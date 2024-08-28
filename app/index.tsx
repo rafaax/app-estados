fd2cf85efa47d3ef68026d701db7165a9df467e1
raphael <rafavfx1@gmail.com>
2024-08-28 20:24:33 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {  View } from "react-native";
+import { Botao } from '@/components/Botao';
+
+export default function Index() {
+  return (
+    <View>
+      <Botao></Botao>
+    </View>
+  );
+}
---
+++ components/Botao.tsx
@@ -0,0 +1,9 @@
+import { TouchableOpacity, Text } from "react-native";
+
+export function Botao() {
+    return (
+        <TouchableOpacity style={{backgroundColor: 'green', width: 50}}>
+            <Text>Botão</Text>  
+        </TouchableOpacity>
+    )
+}
